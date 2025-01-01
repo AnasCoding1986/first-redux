0c0eec7f556a99dc99485a373678b9d5bb1e1bf6
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2025-01-01 09:23:51 +0600

    redux first module end

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { RootState } from "./redux/store";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 
 function App() {
+
+  const dispatch = useAppDispatch();
+  const {count} = useAppSelector((state:RootState) => state.counter);
+
+  const handleIncrement = (amount:number)=> {
+    dispatch(increment(amount))
+  }
+
+  const handleDecrement = ()=> {
+    dispatch(decrement())
+  }
 
   return (
     <>
 
       <h1>Counter with Redux</h1>
 
-      <button>Increment</button>
-      <div>0</div>
-      <button>Decrement</button>
+      <button onClick={()=>handleIncrement(1)}>Increment by 1</button>
+      <button onClick={()=>handleIncrement(5)}>Increment by 5</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
 
     </>
   )
